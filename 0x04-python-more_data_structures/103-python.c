c04c21e2d51ec643b0250ff5c98019273db4d1b3
Kryuell <Kryuel17@gmail.com>
2023-12-06 20:37:26 +0100

    advanced

--- 0x04-python-more_data_structures/103-python.c
+++ 0x04-python-more_data_structures/103-python.c
@@ -9,13 +9,15 @@ void print_python_bytes(PyObject *p) {
         return;
     }
 
-    Py_ssize_t size = PyBytes_Size(p);
+    Py_ssize_t size = ((PyVarObject *)p)->ob_size;
+    char *data = ((PyBytesObject *)p)->ob_sval;
+
     printf("  size: %ld\n", size);
-    printf("  trying string: %s\n", PyBytes_AsString(p));
+    printf("  trying string: %s\n", data);
 
     printf("  first 10 bytes: ");
     for (Py_ssize_t i = 0; i < 10 && i < size; ++i) {
-        printf("%02x ", (unsigned char)PyBytes_AsString(p)[i]);
+        printf("%02x ", (unsigned char)data[i]);
     }
     printf("\n");
 }
@@ -31,12 +33,12 @@ void print_python_list(PyObject *p) {
         return;
     }
 
-    size = PyList_Size(p);
+    size = ((PyVarObject *)p)->ob_size;
     printf("[*] Size of the Python List = %ld\n", size);
     printf("[*] Allocated = %ld\n", ((PyListObject *)p)->allocated);
 
     for (i = 0; i < size; ++i) {
-        element = PyList_GetItem(p, i);
+        element = ((PyListObject *)p)->ob_item[i];
         printf("Element %ld: ", i);
 
         if (PyBytes_Check(element)) {
