b02e7c75a83ca30844b97e838a4eda8098715c16
Kryuell <Kryuel17@gmail.com>
2023-12-19 17:49:55 +0100

    Mandatory

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -12,5 +12,18 @@ class Square:
         else:
             self.__size = size
 
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if type(value) != int:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
     def area(self):
         return self.__size * self.__size
