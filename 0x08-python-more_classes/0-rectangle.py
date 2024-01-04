d4801594e8b5c5fce0fb1cdb77574ef3ac7a4ec0
Kryuell <Kryuel17@gmail.com>
2024-01-04 17:25:52 +0100

    Mandatory

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -4,6 +4,8 @@ This is the "Rectangle"  module.
 
 This module provides a simple Rectangle class.
 """
+
+
 class Rectangle:
     """ This is a placeholder class for representing rectangles."""
     pass
