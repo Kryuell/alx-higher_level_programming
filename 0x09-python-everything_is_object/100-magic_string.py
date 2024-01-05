825a11fd20c7248d2d4b0f5ae3397b469a1f815c
Kryuell <Kryuel17@gmail.com>
2024-01-05 14:16:54 +0100

    Advanced

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 
-def magic_string():
-    magic_string.count = getattr(magic_string, 'count', 0) + 1
-    return "BestSchool" + (", BestSchool" * (magic_string.count - 1))
-
+def magic_string(string=None):
+    if string is None:
+        string = ["BestSchool"]
+    else:
+        string.append("BestSchool")
+    return ", ".join(string)
