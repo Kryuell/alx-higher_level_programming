593de13c97a0592d92a95e63866353ab570270ac
Kryuell <Kryuel17@gmail.com>
2024-01-08 15:36:27 +0100

    Mandatory

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,16 +1,9 @@
 #!/usr/bin/python3
-
 class MyList(list):
     """
     MyList class inherits from list and includes a public method print_sorted().
     """
 
-
-    def __init__(self):
-        """
-        Initializes an empty MyList.
-        """
-        super().__init__()
 
     def print_sorted(self):
         """
