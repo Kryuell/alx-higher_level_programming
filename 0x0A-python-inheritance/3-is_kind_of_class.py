eb250fdf086d34eddc20905028278c9e5f0fde5b
Kryuell <Kryuel17@gmail.com>
2024-01-08 16:03:16 +0100

    mandatory

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-"""Check if an object is an instance of, or inherited from, a specified class."""
+"""Check if an object is an instance of, or inherited from,
+ a specified class."""
 
 
 def is_kind_of_class(obj, a_class):
