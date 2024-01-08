d8f923b726ba23f029187f77aa4af72f857d963e
Kryuell <Kryuel17@gmail.com>
2024-01-08 17:45:49 +0100

    Mandatory

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -35,12 +35,14 @@ def add_integer(a, b=98):
         ...
     TypeError: a must be an integer
     """
+
     if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
     if not isinstance(b, (int, float)):
         raise TypeError("b must be an integer")
 
     return int(a) + int(b)
+
 
 if __name__ == "__main__":
     import doctest
