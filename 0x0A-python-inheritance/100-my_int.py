9faae9aff869eb277d4717d77822c2fbe6a39e99
Kryuell <Kryuel17@gmail.com>
2024-01-08 17:16:28 +0100

    Mandatory

---
+++ 0x0A-python-inheritance/100-my_int.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Class MyInt inherits from int with inverted == and != operators."""
+
+class MyInt(int):
+    """Overrides equality (==) operator."""
+    def __eq__(self, other):
+        return super().__ne__(other)
+
+    """Overrides inequality (!=) operator."""
+    def __ne__(self, other):
+        return super().__eq__(other)
