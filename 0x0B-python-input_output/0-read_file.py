4298f1bf72f271406f9d293df74b427eb567fb28
Kryuell <Kryuel17@gmail.com>
2024-01-09 16:07:54 +0100

    Mandatory

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
 
-def read_file(filename=""):
-    with open(filename, "r", encoding="utf-8") as file:
-        print(file.read(), end='')
+def number_of_lines(filename=""):
+    with open(filename, "r", encoding="UTF-8") as f:
+        return len(list(f))
