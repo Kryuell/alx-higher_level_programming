7540a9929e8e6a5bc5099ccc6453518ab2a78194
Kryuell <Kryuel17@gmail.com>
2024-01-09 20:32:09 +0100

    Mandatory

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Defines a JSON-to-object function."""
+
+
+def from_json_string(my_str):
+    """Returns an object (Python data structure) represented by a JSON string."""
+    import json
+    return json.loads(my_str)
