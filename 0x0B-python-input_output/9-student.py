36a4b332b6400c2a34c5e9df01c986c8874d6b1d
Kryuell <Kryuel17@gmail.com>
2024-01-09 21:22:42 +0100

    Mandatory

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -1,28 +1,22 @@
 #!/usr/bin/python3
-"""Student model """
+"""Defines a class Student."""
 
 
 class Student:
-    """
-    Defines a student with first_name, last_name, and age.
-    """
+    """Represent a student."""
 
     def __init__(self, first_name, last_name, age):
-        """
-        Initializes a Student instance with specified attributes.
+        """Initialize a new Student.
+
+        Args:
+            first_name (str): The first name of the student.
+            last_name (str): The last name of the student.
+            age (int): The age of the student.
         """
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
-
-def to_json(self):
-    """
-    Retrieves a dictionary representation
-    of a Student instance.
-    """
-    return {
-        'first_name': self.first_name,
-        'last_name': self.last_name,
-        'age': self.age
-    }
+    def to_json(self):
+        """Get a dictionary representation of the Student."""
+        return self.__dict__
