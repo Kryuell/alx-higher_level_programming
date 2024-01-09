179a34146380d33c6a392f8bb3a07ef276488f9f
Kryuell <Kryuel17@gmail.com>
2024-01-09 20:32:49 +0100

    Mandatory

--- 0x0B-python-input_output/4-from_json_string.py
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -3,6 +3,7 @@
 
 
 def from_json_string(my_str):
-    """Returns an object (Python data structure) represented by a JSON string."""
+    """Returns an object (Python data structure)
+     represented by a JSON string."""
     import json
     return json.loads(my_str)
