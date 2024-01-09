86355c1cf53fc03f3e9860d9ac6ba06aec563a37
Kryuell <Kryuel17@gmail.com>
2024-01-09 20:24:47 +0100

    Mandatory

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -3,7 +3,7 @@
 
 
 def append_write(filename="", text=""):
-    """Appends a string at the end of a text file (UTF8) 
+    """Appends a string at the end of a text file (UTF8)
     and returns the number of characters added."""
     with open(filename, 'a', encoding='utf-8') as file:
         nb_characters_added = file.write(text)
