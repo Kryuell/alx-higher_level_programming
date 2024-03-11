1bb71134d09d698efbdd51000369da52bff3d8e6
Kryuell <Kryuel17@gmail.com>
2024-03-11 22:01:33 +0000

    Js

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+
+function factorial (num) {
+  if (isNaN(num)) {
+    return 1;
+  }
+  if (num === 0) {
+    return 1;
+  } else {
+    return num * factorial(num - 1);
+  }
+}
+
+console.log(factorial(parseInt(process.argv[2])));
