cb85da3e539de2a44855747f70b942a870573a5b
Kryuell <Kryuel17@gmail.com>
2024-03-29 16:12:12 +0100

    web_stack_deb

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
-
-url=$1
-
-# Send a GET request to the URL and extract the body size from the response headers
-body_size=$(curl -s -I $url | grep -i "content-length" | awk '{print $2}')
-
-echo $body_size
+# cURL b0dy siz3
+curl -sw '%{size_download}\n' -o /dev/null "$1"
