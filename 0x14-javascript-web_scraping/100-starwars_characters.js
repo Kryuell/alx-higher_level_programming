8f27c64e1e904133dad7fb9e882cd7b42b453489
Kryuell <Kryuel17@gmail.com>
2024-04-23 21:40:37 +0200

    web_scraping

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -1,28 +1,13 @@
 #!/usr/bin/node
-
 const request = require('request');
+const id = process.argv[2];
 
-const movieId = process.argv[2];
-const apiUrl = `https://swapi.dev/api/films/${movieId}/`;
-
-request(apiUrl, function (error, response, body) {
-  if (!error && response.statusCode === 200) {
-    const movieData = JSON.parse(body);
-
-    console.log(`Characters of "${movieData.title}":`);
-
-    movieData.characters.forEach((characterUrl) => {
-      request(characterUrl, function (charError, charResponse, charBody) {
-        if (!charError && charResponse.statusCode === 200) {
-          const characterData = JSON.parse(charBody);
-
-          console.log(characterData.name);
-        } else {
-          console.error('Error fetching character data:', charError);
-        }
-      });
+request(`https://swapi-api.alx-tools.com/api/films/${id}`, { json: true }, (err, res, body) => {
+  if (err) console.log(err);
+  for (const character of body.characters) {
+    request(character, { json: true }, (err, res, body) => {
+      if (err) console.log(err);
+      console.log(body.name);
     });
-  } else {
-    console.error('Error fetching movie data:', error);
   }
 });
--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,44 +1,18 @@
 #!/usr/bin/node
-
 const request = require('request');
+const util = require('util');
+const id = process.argv[2];
 
-const movieId = process.argv[2];
+const prequest = util.promisify(request);
 
-const apiUrl = `https://swapi.dev/api/films/${movieId}/`;
-
-//  the 'request' module to perform an HTTP GET request to the Star Wars API URL.
-request(apiUrl, function (error, response, body) {
-  // Check if there was no error during the HTTP request
-  if (!error && response.statusCode === 200) {
-    // Parse the JSON response bod
-    const movieData = JSON.parse(body);
-    // create an array of promises that fetch the data for each individual character.
-    const characterPromises = movieData.characters.map((characterUrl) => {
-      return new Promise((resolve, reject) => {
-        // Use another 'request' to fetch the data for the individual character.
-        request(characterUrl, function (charError, charResponse, charBody) {
-          // Check if there was no error during the HTTP request
-          if (!charError && charResponse.statusCode === 200) {
-            // Parse the JSON response body
-            const characterData = JSON.parse(charBody);
-            // Resolve the promise with the name of the character.
-            resolve(characterData.name);
-          } else {
-            // reject the promise with the error message if  there was an error during the HTTP request
-            reject(new Error(`Error fetching character data: ${charError}`));
-          }
-        });
-      });
-    });
-
-    Promise.all(characterPromises)
-      .then((characterNames) => {
-        console.log(characterNames.join('\n'));
-      })
-      .catch((error) => {
-        console.error(error.message);
-      });
-  } else {
-    console.error('Error fetching movie data:', error);
+(async () => {
+  try {
+    const body = (await prequest(`https://swapi-api.alx-tools.com/api/films/${id}`, { json: true })).body;
+    for (const url of body.characters) {
+      const body = (await prequest(url, { json: true })).body;
+      console.log(body.name);
+    }
+  } catch (err) {
+    console.log(err);
   }
-});
+})();
